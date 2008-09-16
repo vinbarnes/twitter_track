61822e82a6f9fe7cf57448c6df6eb70154a7632a
Kevin R. Barnes <vinbarnes@gmail.com>
2008-09-16 13:35:48 -0500

    Add meat

---
+++ twitter_track.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'appscript'
+include Appscript
+
+USER   = '<your_name_goes_here>'
+PASSWD = '<your_topsecret_password_goes_here>'
+
+itunes = app('iTunes')
+track  = itunes.current_track.name.get
+artist = itunes.current_track.artist.get
+post   = "listening to '#{track}' by #{artist}"
+result = %x[curl -u #{USER}:#{PASSWD} -d status="#{post}" http://twitter.com/statuses/update.json]
